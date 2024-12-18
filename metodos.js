b3806b8d6ac977e182e85f582c732445bcd4b20e
Gerardo Quizhpe <99355032+GerardoQuizhpe@users.noreply.github.com>
2024-12-18 15:19:19 -0500

    actualización final

--- metodos.js
+++ metodos.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent('interaction-handler', {
     schema: {
         rotationSpeed: { type: 'number', default: 0.5 },
         zoomSpeed: { type: 'number', default: 0.1 },
-        moveSpeed: { type: 'number', default: 0.01 }, // Velocidad de movimiento adicional
+        moveSpeed: { type: 'number', default: 0.01 },
         minScale: { type: 'number', default: 0.1 },
         maxScale: { type: 'number', default: 5 }
     },
@@ -14,19 +14,19 @@ AFRAME.registerComponent('interaction-handler', {
             prevPos: {},
             initDist: 0,
             scale: this.data.minScale,
-            lastClickTime: 0 // Para detectar el doble click
+            lastClickTime: 0
         };
         this.bindEvents();
     },
     bindEvents: function () {
         const sceneEl = this.el.sceneEl;
         const events = {
-            mousemove: e => this.handleInteraction(e.clientX, e.clientY, 'rotate'), // Manejar rotación con el mouse
-            mousedown: e => this.startInteraction(e.clientX, e.clientY, 'mouse'), // Iniciar rotación o movimiento con el mouse
-            mouseup: () => this.resetState(), // Finalizar rotación o movimiento
-            touchstart: e => this.handleTouchStart(e), // Manejar inicio de toques
-            touchmove: e => this.handleTouchMove(e), // Manejar movimientos táctiles
-            touchend: () => this.resetState(), // Reiniciar estado al soltar los toques
+            mousemove: e => this.handleInteraction(e.clientX, e.clientY), // Manejar interacción según el estado actual
+            mousedown: e => this.startInteraction(e.clientX, e.clientY, 'mouse'), // Detectar inicio de interacción con clic
+            mouseup: () => this.resetState(), // Finalizar interacción al soltar el clic
+            touchstart: e => this.handleTouchStart(e), // Detectar inicio de interacción táctil
+            touchmove: e => this.handleTouchMove(e), // Manejar interacción táctil
+            touchend: () => this.resetState(), // Finalizar interacción táctil
             wheel: e => this.zoom(e.deltaY * -1) // Manejar zoom con la rueda del mouse
         };
         Object.entries(events).forEach(([type, handler]) => sceneEl.addEventListener(type, handler));
@@ -48,34 +48,34 @@ AFRAME.registerComponent('interaction-handler', {
         if (timeDifference < 300) {
             this.setState('rotating', true); // Doble clic para rotar
         } else {
-            this.setState('moving', { x, y }); // Click simple para mover
+            this.setState('moving', { x, y }); // Clic simple para mover
         }
     },
-    handleInteraction: function (x, y, mode) {
-        if (mode === 'rotate' && !this.state.rotating) return;
-
+    handleInteraction: function (x, y) {
+        if (this.state.rotating) {
+            this.rotate(x, y); // Realizar rotación
+        } else if (this.state.moving) {
+            this.move(x, y); // Realizar movimiento
+        }
+        this.state.prevPos = { x, y }; // Actualizar la posición previa
+    },
+    rotate: function (x, y) {
         const { x: prevX, y: prevY } = this.state.prevPos;
         const rotation = this.el.getAttribute('rotation');
-        if (this.state.rotating) {
-            this.el.setAttribute('rotation', {
-                x: rotation.x - (y - prevY) * this.data.rotationSpeed,
-                y: rotation.y + (x - prevX) * this.data.rotationSpeed,
-                z: rotation.z
-            });
-        } else if (this.state.moving) {
-            this.move(x, y);
-        }
-        this.state.prevPos = { x, y };
+        this.el.setAttribute('rotation', {
+            x: rotation.x - (y - prevY) * this.data.rotationSpeed,
+            y: rotation.y + (x - prevX) * this.data.rotationSpeed,
+            z: rotation.z
+        });
     },
     move: function (x, y) {
         const { x: prevX, y: prevY } = this.state.prevPos;
         const position = this.el.getAttribute('position');
         this.el.setAttribute('position', {
             x: position.x + (x - prevX) * this.data.moveSpeed,
-            y: position.y + (y - prevY) * this.data.moveSpeed,
+            y: position.y - (y - prevY) * this.data.moveSpeed, // Invertido para movimiento natural
             z: position.z
         });
-        this.state.prevPos = { x, y };
     },
     handleTouchStart: function (e) {
         if (e.touches.length === 1) {
@@ -83,15 +83,19 @@ AFRAME.registerComponent('interaction-handler', {
         } else if (e.touches.length === 2) {
             this.state.zooming = true;
             this.state.initDist = this.getTouchDistance(e.touches);
+        } else if (e.touches.length === 3) {
+            this.setState('moving', { x: e.touches[0].clientX, y: e.touches[0].clientY });
         }
     },
     handleTouchMove: function (e) {
-        if (e.touches.length === 1) {
-            this.handleInteraction(e.touches[0].clientX, e.touches[0].clientY, 'rotate');
+        if (e.touches.length === 1 && this.state.rotating) {
+            this.handleInteraction(e.touches[0].clientX, e.touches[0].clientY);
         } else if (e.touches.length === 2 && this.state.zooming) {
             const dist = this.getTouchDistance(e.touches);
             this.zoom((dist - this.state.initDist) * this.data.zoomSpeed);
             this.state.initDist = dist;
+        } else if (e.touches.length === 3 && this.state.moving) {
+            this.handleInteraction(e.touches[0].clientX, e.touches[0].clientY);
         }
     },
     getTouchDistance: function (touches) {
